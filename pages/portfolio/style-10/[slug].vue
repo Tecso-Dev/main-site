a1fef17a812e27ac83948cbe0fee224721f393a1
sobhanaz <sobhan.gjhav.azimzadeh@gmail.com>
2025-05-15 01:51:45 +0330

    add porfolio detail

--- pages/portfolio/style-10/[slug].vue
+++ pages/portfolio/style-10/[slug].vue
@@ -1,26 +1,31 @@
 <script setup>
-import { nextProject } from "@/hooks/plugin.js"
+import { nextProject } from "@/hooks/plugin.js";
 import { Fancybox } from "@fancyapps/ui";
 import data_Portf from "@/api/portfolio/portfolio.json";
 import InfoProject from "@/components/portfolio/info-project/InfoProject.vue";
 import InfoProjectItem from "@/components/portfolio/info-project/InfoProjectItem.vue";
 import ParallaxImageTwo from "@/components/images/ParallaxImageTwo.vue";
+import ViewVideo from "@/components/images/ViewVideo.vue";
 import ImagePopup from "@/components/images/ImagePopup.vue";
-import BoxInfoFull from "@/components/box-info/BoxInfoFull.vue";
+import BoxInfoRight from "@/components/box-info/BoxInfoRight.vue";
 import ParagraphBox from "@/components/string/ParagraphBox.vue";
 import NextProject from "@/components/next/NextProject.vue";
-import ViewVideo from "@/components/images/ViewVideo.vue";
-
 definePageMeta({ documentDriven: false });
-const { slug } = useRoute().params
-const item = await data_Portf.find(item => item.slug === slug)
+const { slug } = useRoute().params;
+const item = await data_Portf.find((item) => item.slug === slug);
 if (!item) {
     throw createError({
-        fatal: true, statusCode: 404,
+        fatal: true,
+        statusCode: 404,
         statusMessage: "Work Not Found",
-        data: { msg: useRoute().path, redirectLink: "/work/work-list-1", linkName: "Portfolio" }
-    })
+        data: {
+            msg: useRoute().path,
+            redirectLink: "/work/work-list-1",
+            linkName: "Portfolio",
+        },
+    });
 }
+
 onMounted(() => {
     Fancybox.defaults.Hash = false;
     Fancybox.bind("[data-fancybox]", {
@@ -28,26 +33,38 @@ onMounted(() => {
         Toolbar: {
             display: {
                 left: ["infobar"],
-                middle: ["zoomIn", "zoomOut", "toggle1to1", "rotateCCW", "rotateCW", "flipX", "flipY",],
+                middle: [
+                    "zoomIn",
+                    "zoomOut",
+                    "toggle1to1",
+                    "rotateCCW",
+                    "rotateCW",
+                    "flipX",
+                    "flipY",
+                ],
                 right: ["slideshow", "thumbs", "close"],
             },
         },
         Carousel: { infinite: true },
     });
-})
-onUpdated(() => Fancybox.close())
+});
+onUpdated(() => Fancybox.close());
 onBeforeUnmount(() => Fancybox.destroy());
 </script>
 
 <template>
-
     <Head>
-        <Title>Tecso Platform Development</Title>
+        <Title>{{ item.title }}</Title>
     </Head>
 
     <Layout>
         <!-- ========== Header  ========== -->
-        <HeaderFull :data="item" boxShadow btnScrollTo="info" :linkCircleTo="item.autherLink" />
+        <HeaderFull
+            :data="item"
+            boxShadow
+            btnScrollTo="info"
+            :linkCircleTo="item.autherLink"
+        />
         <!-- ========== End Header  ========== -->
 
         <!-- ========== Info Project  ========== -->
@@ -60,41 +77,48 @@ onBeforeUnmount(() => Fancybox.destroy());
                     {{ item.about[0].text[0] }}
                 </p>
             </template>
-            <InfoProjectItem v-for="(info, index) in item.infoProject" :key="index" :title="info.title"
-                :description="info.description" />
+            <InfoProjectItem
+                v-for="(info, index) in item.infoProject"
+                :key="index"
+                :title="info.title"
+                :description="info.description"
+            />
         </InfoProject>
         <!-- ========== End Info Project  ========== -->
 
-        <!-- ========== Parallax Image With Popup  ========== -->
-        <ParallaxImageTwo :src="item.images[1].src" :caption="item.images[1].name" fancybox="_1" />
-        <!-- ========== End Parallax Image With Popup  ========== -->
+        <!-- ========== video  ========== -->
         <ViewVideo
             class="scale"
             :src="item.video[0].src"
             :poster="item.images[1].src"
         />
-
-        <!-- ========== Paragraph  ========== -->
-        <ParagraphBox>{{ item.about[1].text[0] }}</ParagraphBox>
-        <!-- ========== End Paragraph  ========== -->
+        <ParallaxImageTwo
+            :src="item.images[4].src"
+            :caption="item.images[4].name"
+            fancybox="_1"
+        />
+        <!-- ========== End video  ========== -->        <!-- ========== Info  ========== -->
+        <BoxInfoRight v-if="item.about[2]" :data="item.about[2]" />
+        <!-- ========== End Info  ========== -->
 
         <!-- ========== Image Popup  ========== -->
-        <ImagePopup class="dsn-container" :imageList="[item.images[2], item.images[3]]" />
+        <ImagePopup
+            class="dsn-container"
+            mask
+            :imageList="[item.images[1], item.images[3]]"
+        />
         <!-- ========== End Image Popup  ========== -->
 
         <!-- ========== Parallax Image With Popup  ========== -->
-        <ParallaxImageTwo :src="item.images[6].src" :caption="item.images[6].name" fancybox="_1" />
+        <ParallaxImageTwo
+            :src="item.images[2].src"
+            :caption="item.images[2].name"
+            fancybox="_1"
+        />
         <!-- ========== End Parallax Image With Popup  ========== -->
 
-        <!-- ========== Image Popup  ========== -->
-        <ImagePopup class="dsn-container" :imageList="[item.images[4], item.images[5]]" />
-        <!-- ========== End Image Popup  ========== -->
-
         <!-- ========== Paragraph  ========== -->
-        <ParagraphBox>
-            {{ item.about[1].text[1] }}
-            <h5 class="title-block dsn-up mt-15">Tecso Team</h5>
-        </ParagraphBox>
+        <ParagraphBox>{{ item.about[1].text[1] }}</ParagraphBox>
         <!-- ========== End Paragraph  ========== -->
 
         <!-- ========== Next Project  ========== -->
